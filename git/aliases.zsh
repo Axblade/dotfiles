38f3b68bae289417f8f771e41ecd771d13e81739
Eugene Sypachev <eugene.sypachev@gmail.com>
2012-10-22 00:56:59 +0400

    git aliases

---
+++ git/aliases.zsh
@@ -0,0 +1,16 @@
+hub_path=$(which hub)
+if [[ -f $hub_path ]]
+then
+  alias git=$hub_path
+fi
+
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' 
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
