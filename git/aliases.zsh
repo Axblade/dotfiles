6301ae59bb3a8288e3fc7300e95f59f573c49c89
Eugene Sypachev <eugene.sypachev@gmail.com>
2012-10-22 22:08:44 +0400

    up

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,13 +4,13 @@ then
   alias git=$hub_path
 fi
 
-alias gl='git pull --prune'
+alias gl="git pull --prune"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias lol = 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' 
+alias glol="git log --graph --decorate --pretty=oneline --abbrev-commit --all"
+alias gp="git push origin HEAD"
+alias gd="git diff"
+alias gc="git commit"
+alias gca="git commit -a"
+alias gco="git checkout"
+alias gb="git branch"
+alias gs="git status -sb"
