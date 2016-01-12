0604d2b7990a0e9307004f92daf6018aeb4fa04a
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-12 12:27:49 +0300

    update mac alias for ST3

--- sublime/aliases.zsh
+++ sublime/aliases.zsh
@@ -2,6 +2,6 @@ if [[ $('uname') == 'Linux' ]]; then
   runst() { nohup /usr/bin/sublime-text-2 $@ > /dev/null & }
   alias st=runst
 elif  [[ $('uname') == 'Darwin' ]]; then
-  alias st='open -a /Applications/Sublime\ Text\ 2.app'
+  alias st='open -a /Applications/Sublime\ Text.app'
 fi
 alias stt='st .'
